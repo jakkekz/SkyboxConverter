e6c86c174082dc76d7b2a1ede4d752b346054990
jaakko <120755650+jakkekz@users.noreply.github.com>
2025-10-16 20:05:54 +0300

    now?

--- hook-PIL.py
+++
@@ -1,6 +0,0 @@
-# PyInstaller Hook for Pillow (PIL)
-from PyInstaller.utils.hooks import collect_data_files
-
-# This collects all data files (like C libraries and support files) 
-# that Pillow relies on and ensures they are bundled.
-datas = collect_data_files('PIL')
